6cb2b43987fb22483e4b346851574b49119b14f1
TanayHingane <tanayhingane03@gmail.com>
2025-03-11 19:52:09 +0530

    temp push
    done

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <FloatingNav
           navItems={[
             { name: "Home", link: "/" },
-            { name: "About Us", link: "/" },
+            { name: "Projects", link: "/" },
           ]}
         />
         <Hero />

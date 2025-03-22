a8b3a1a6f2bd4288177f8e7498bf048b620fe511
Tanay Hingane <157800375+TanayHingane@users.noreply.github.com>
2025-03-22 18:43:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/tpfp.jpg" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
